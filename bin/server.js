74db7a15ac279293363c47f583dc0c9ca4567283
KatePodolsky <Doogmatix91@gmail.com>
2021-10-22 14:19:45 +0300

    Fix

--- bin/server.js
+++ bin/server.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const app = require('../app')
 
-const { DB_HOST, PORT = 4000 } = process.env
+const { DB_HOST, PORT = 4000 } = process.env;
 
-mongoose.connect(DB_HOST, {
+mongoose
+  .connect(DB_HOST, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
   .then(() => {
-    app.listen(PORT, () => {
-  console.log(`Database connection successful`)
-});
-  })
+    app.listen(PORT);
+    console.log(`Database connection successful`);
+})
   .catch((error) => {
     console.log(error.message);
     process.exit(1)
